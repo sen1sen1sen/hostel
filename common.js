ff8d7dd04e4736a1cb2f84dbb85f893b3a0fc2ae
sen1sen1sen <49861026+sen1sen1sen@users.noreply.github.com>
2019-09-02 12:43:57 +0300

    Add files via upload

---
+++ common.js
@@ -0,0 +1,65 @@
+ function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
+}
+
+$(document).ready(function(){
+	$(".tabs").lightTabs();
+});
+
+(function($){				
+	jQuery.fn.lightTabs = function(options){
+
+		var createTabs = function(){
+			tabs = this;
+			i = 0;
+			
+			showPage = function(i){
+				$(tabs).children("div").children("div").hide();
+				$(tabs).children("div").children("div").eq(i).show();
+				$(tabs).children("ul").children("li").removeClass("active");
+				$(tabs).children("ul").children("li").eq(i).addClass("active");
+			}
+								
+			showPage(0);				
+			
+			$(tabs).children("ul").children("li").each(function(index, element){
+				$(element).attr("data-page", i);
+				i++;                        
+			});
+			
+			$(tabs).children("ul").children("li").click(function(){
+				showPage(parseInt($(this).attr("data-page")));
+			});				
+		};		
+		return this.each(createTabs);
+	};	
+})(jQuery);
+
+
+
+ $('.owl-carousel').owlCarousel({
+    loop: true,
+    items: 1, 
+    margin: 10,
+    nav: true,
+    autoplay: true,
+    autoplayTimeout: 7000,
+    autoplayHoverPause: false,
+    responsive:{
+        485:{
+            items: 1
+        },
+        600:{
+            items: 3
+        },
+        1025:{
+            items: 4
+        }
+    }
+})
+ 
